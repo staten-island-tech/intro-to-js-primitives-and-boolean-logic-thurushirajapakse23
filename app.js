d6adb851e904b1d07254008c318570cfb29e5b3a
thurushirajapakse23 <56314451+thurushirajapakse23@users.noreply.github.com>
2020-04-01 09:53:29 -0400

    heryh

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ if(light === "green"){
     console.log("Stop");
 }
  */
-
+/* 
 let kristyIntolerant = true;
 let baoBaoIntolerant = false;
 if(kristyIntolerant === true && baoBaoIntolerant === true){
@@ -83,4 +83,90 @@ if (temperature < 68){
     console.log("Warm");
 }else{
     console.log ("Too hot");
+} */
+
+/* function test(){
+    console.log("I'm a function");
 }
+
+test();
+ */
+
+/*  const add = function(x,y) {
+    return x + y; ///return ends the function
+ };
+
+console.log(add(100,20));
+const name = "Richard"; /////cannot re-define const
+ */
+/* 
+ const hello = function(){
+     console.log("Hello")
+ };
+ const bye = function(){
+    console.log("Bye")
+};
+const hiBye = function(){
+    hello();
+    bye();
+    hello();
+    bye();
+};
+hiBye(); */
+
+//const name = "Thurushi"; //golbal variabe or global space
+
+/* function displayName(){
+    const name = "Kelly"; //local or function scope
+    function sun(){
+        console.log(name); //inner function has access to outer function
+    }
+    return sun;
+}
+
+const myFunc = displayName();
+myFunc();
+//closure */
+
+////////Arrays
+
+/* const names = ["Aarthi", "Vicky", "Vincent", "Doyle", "Kelly"];
+names.forEach(name => console.log(name));
+ */
+
+ ////spread operator
+
+/*  const numbers = [1,2,3,4,5,6];
+ const add = function(x,y,z,a,b,c){
+     return x + y + z + a + b + c;
+ };
+  console.log(add(...numbers)) */
+
+let students = ["Riya", "Omor", "Alan", "Jaiyu", "Michael", "Lisa", "Richard"];
+let result = students.filter(student => student.length > 4)
+//console.log(result);
+
+
+///filter out all students whose names contain less than 5 characters
+/* const newArr = students
+    .push()
+    .forEach()
+    .map(); */
+
+////"includes" junshen
+
+console.log(students.includes("Junshen"));
+console.log(students.includes("at"));
+
+
+///Alan or Sava included in "some"
+function check(element, array){
+    return array.some
+}
+
+const checkSava = check("Sava", students);
+const checkAlan = check("Alan", students);
+
+console.log(checkAlan +  checkSava);
+
+array.some(element => )
